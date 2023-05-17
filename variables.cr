803ca0a8d52972c9a9e1de09080fbed15feefdc2
Kensuke Nagae <kyanny@github.com>
2023-05-17 15:18:43 +0000

    Intro - variables

---
+++ variables.cr
@@ -0,0 +1,27 @@
+message = "Hello Penny!"
+
+puts message
+puts message
+puts message
+
+message = "Hello Penny!"
+
+p! typeof(message)
+p typeof(message)
+
+message = "Hello Penny!"
+
+p! message
+
+message = "Hello Sheldon!"
+
+p! message
+
+
+message = "Hello Penny!"
+
+p message, typeof(message)
+
+message = 73
+
+p! message, typeof(message)
