d3352136bd372103c252399d8f8a3a3c06d25559
Kensuke Nagae <kyanny@github.com>
2023-05-18 15:05:39 +0000

    Intro - strings

---
+++ strings.cr
@@ -0,0 +1,20 @@
+name = "Crystal"
+puts "Hello #{name}"
+
+name = 6
+puts "Hello #{name}!"
+
+# puts "Hello" + name + "!"
+
+name = "Crystal"
+puts "Hello" + name + "!"
+
+puts "I say: \"Hello world\""
+
+puts "newline\ntab\tcharacter and null\0character and backslash\\"
+
+puts %(<div class="hello">hello</div>)
+puts %{<div class="hello">hello</div>}
+puts %[<div class="hello">hello</div>]
+puts %|<div class="hello">hello</div>|
+
